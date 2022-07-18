538cfd868f353841539d6f3d7973991aa7ecc16e
Tatiana Rico <tatianarico1702@gmail.com>
2022-07-18 20:09:02 -0300

    add projeto de clean code

---
+++ DeliveryAppChallenge/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  DeliveryAppChallenge
+//
+//  Created by Rodrigo Borges on 25/10/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard(name: "User", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+        self.window?.rootViewController = UINavigationController(rootViewController: vc)
+        self.window?.windowScene = windowScene
+        self.window?.makeKeyAndVisible()
+    }
+}
+
